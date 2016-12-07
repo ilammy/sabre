b8a54d8e5836df87f0dbd9727ad1e3c7c8a9c303
ilammy <a.lozovsky@gmail.com>
2016-12-07 02:23:25 +0200

    new new definition
    now with fewer lifetimes

--- src/libreader/tests/utils/tree.rs
+++ src/libreader/tests/utils/tree.rs
@@ -132,7 +132,7 @@ pub trait TreeNode<'a> where Self: 'a {
 }
 
 /// Trait of tree nodes.
-pub trait TreeNodeEx<'a> where Self: Sized {
+pub trait TreeNodeEx where Self: Sized {
     /// Value of this node.
     type Value;
 
@@ -140,8 +140,8 @@ pub trait TreeNodeEx<'a> where Self: Sized {
     type ChildIter: Iterator<Item=Self>;
 
     /// Returns value of this node.
-    fn value(&'a self) -> &'a Self::Value;
+    fn value(&self) -> &Self::Value;
 
     /// Returns iterator over child nodes of this node.
-    fn children(&'a self) -> Self::ChildIter;
+    fn children(&self) -> Self::ChildIter;
 }
