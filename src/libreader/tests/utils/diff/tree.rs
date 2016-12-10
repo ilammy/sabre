d299ca081ee5ba6a1e646bc2c824376e0550003f
ilammy <a.lozovsky@gmail.com>
2016-12-10 20:53:21 +0200

    more stupid changes

--- src/libreader/tests/utils/diff/tree.rs
+++ src/libreader/tests/utils/diff/tree.rs
@@ -71,16 +71,13 @@ pub enum Diff<'a, T> where T: 'a {
 
     /// Corresponding nodes are equal (and we can compare their children).
     Equal(&'a T, &'a T, Vec<Diff<'a, T>>),
-
-    /// Corresponding nodes are not equal.
-    Replace(&'a T, &'a T),
 }
 
 impl<'a, T> TreeNode for Diff<'a, T> {
     fn children<'b>(&'b self) -> Vec<&'b Self> {
         match *self {
             // Non-equal diffs are terminal, they have no child nodes
-            Diff::Left(_) | Diff::Right(_) | Diff::Replace(_, _) => vec![],
+            Diff::Left(_) | Diff::Right(_) => vec![],
 
             // Equal diffs provide access to their child diffs
             Diff::Equal(_, _, ref children) => {
@@ -104,12 +101,6 @@ impl<'a, T> DisplayTreeNode for Diff<'a, T> where T: DisplayTreeNode {
             Diff::Equal(node, _, _) => {
                 try!(node.fmt(f));
             }
-            Diff::Replace(left_node, right_node) => {
-                try!(f.write_str("(-) "));
-                try!(left_node.fmt(f));
-                try!(f.write_str("\n(+) "));
-                try!(right_node.fmt(f));
-            }
         }
         Ok(())
     }
@@ -130,32 +121,28 @@ pub trait ShallowPartialEq<Rhs = Self> {
     fn ne(&self, other: &Rhs) -> bool { !self.eq(other) }
 }
 
-/// Compute the difference between two trees of comparable elements.
-pub fn diff<'a, T>(lhs: &'a T, rhs: &'a T) -> Diff<'a, T>
+/// Compute the difference between two forests of trees of comparable elements.
+pub fn diff<'a, T>(lhs: &'a [T], rhs: &'a [T]) -> Vec<Diff<'a, T>>
     where T: TreeNode + ShallowPartialEq<T>
 {
     diff_with(lhs, rhs, |lhs, rhs| lhs.eq(rhs))
 }
 
-/// Compute the difference between two tree using the provided comparator.
-pub fn diff_with<'a, T, F>(lhs: &'a T, rhs: &'a T, equal: F) -> Diff<'a, T>
+/// Compute the difference between two forests of trees using the provided comparator.
+pub fn diff_with<'a, T, F>(lhs: &'a [T], rhs: &'a [T], equal: F) -> Vec<Diff<'a, T>>
     where T: TreeNode, F: Fn(&T, &T) -> bool
 {
-    if equal(lhs, rhs) {
-        Diff::Equal(lhs, rhs, child_diff(lhs, rhs, &equal))
-    } else {
-        Diff::Replace(lhs, rhs)
-    }
+    // We need this weird repackaging in order to be able to recurse.
+    let lhs = lhs.iter().collect::<Vec<_>>();
+    let rhs = rhs.iter().collect::<Vec<_>>();
+    return child_diff(&lhs, &rhs, &equal);
 }
 
-/// Actually compute a diff between equal child nodes.
-fn child_diff<'a, T, F>(lhs: &'a T, rhs: &'a T, equal: &F) -> Vec<Diff<'a, T>>
+/// Actually compute a diff between forest slices.
+fn child_diff<'a, T, F>(lhs: &[&'a T], rhs: &[&'a T], equal: &F) -> Vec<Diff<'a, T>>
     where T: TreeNode, F: Fn(&T, &T) -> bool
 {
-    let lhs_children = lhs.children();
-    let rhs_children = rhs.children();
-
-    sequence::diff_with(&lhs_children, &rhs_children, |lhs, rhs| equal(lhs, rhs))
+    sequence::diff_with(lhs, rhs, |lhs, rhs| equal(lhs, rhs))
         .iter()
         .map(|diff| match *diff {
             sequence::Diff::Left(lhs) => {
@@ -164,11 +151,9 @@ fn child_diff<'a, T, F>(lhs: &'a T, rhs: &'a T, equal: &F) -> Vec<Diff<'a, T>>
             sequence::Diff::Right(rhs) => {
                 self::Diff::Right(*rhs)
             }
-            sequence::Diff::Replace(lhs, rhs) => {
-                self::Diff::Replace(*lhs, *rhs)
-            }
             sequence::Diff::Equal(lhs, rhs) => {
-                self::Diff::Equal(*lhs, *rhs, child_diff(*lhs, *rhs, equal))
+                self::Diff::Equal(*lhs, *rhs, child_diff(lhs.children().as_slice(),
+                                                         rhs.children().as_slice(), equal))
             }
         })
         .collect()
@@ -180,7 +165,6 @@ pub fn pruned<'a, T>(diff: &Diff<'a, T>) -> Diff<'a, T> {
         // Non-equal nodes have no children and cannot be pruned.
         Diff::Left(lhs) => Diff::Left(lhs),
         Diff::Right(rhs) => Diff::Right(rhs),
-        Diff::Replace(lhs, rhs) => Diff::Replace(lhs, rhs),
 
         // Children of equal nodes should be recursively examined. If their children are all equal
         // nodes with no children (after pruning) then we can prune them as well.
@@ -251,165 +235,180 @@ mod tests {
 
     #[test]
     fn root_not_equal() {
-        let a = Tree::new(1, vec![]);
-        let b = Tree::new(2, vec![]);
+        let a = vec![Tree::new(1, vec![])];
+        let b = vec![Tree::new(2, vec![])];
 
-        assert_eq!(diff(&a, &b), Diff::Replace(&a, &b));
+        assert_eq!(diff(&a, &b), vec![
+            Diff::Left (&a[0]),
+            Diff::Right(&b[0]),
+        ]);
     }
 
     #[test]
     fn root_not_equal_children_equal() {
-        let a = Tree::new(1, vec![Tree::new(2, vec![]), Tree::new(3, vec![])]);
-        let b = Tree::new(2, vec![Tree::new(2, vec![]), Tree::new(3, vec![])]);
+        let a = vec![Tree::new(1, vec![Tree::new(2, vec![]), Tree::new(3, vec![])])];
+        let b = vec![Tree::new(2, vec![Tree::new(2, vec![]), Tree::new(3, vec![])])];
 
-        assert_eq!(diff(&a, &b), Diff::Replace(&a, &b));
+        assert_eq!(diff(&a, &b), vec![
+            Diff::Left (&a[0]),
+            Diff::Right(&b[0]),
+        ]);
     }
 
     #[test]
     fn root_equal() {
-        let a = Tree::new(1, vec![]);
-        let b = Tree::new(1, vec![]);
+        let a = vec![Tree::new(1, vec![])];
+        let b = vec![Tree::new(1, vec![])];
 
-        assert_eq!(diff(&a, &b), Diff::Equal(&a, &b, vec![]));
+        assert_eq!(diff(&a, &b), vec![
+            Diff::Equal(&a[0], &b[0], vec![]),
+        ]);
     }
 
     #[test]
     fn root_equal_children_equal() {
-        let a = Tree::new(1, vec![Tree::new(2, vec![]), Tree::new(3, vec![])]);
-        let b = Tree::new(1, vec![Tree::new(2, vec![]), Tree::new(3, vec![])]);
+        let a = vec![Tree::new(1, vec![Tree::new(2, vec![]), Tree::new(3, vec![])])];
+        let b = vec![Tree::new(1, vec![Tree::new(2, vec![]), Tree::new(3, vec![])])];
 
-        assert_eq!(diff(&a, &b),
-            Diff::Equal(&a, &b, vec![
-                Diff::Equal(&a[0], &b[0], vec![]),
-                Diff::Equal(&a[1], &b[1], vec![]),
+        assert_eq!(diff(&a, &b), vec![
+            Diff::Equal(&a[0], &b[0], vec![
+                Diff::Equal(&a[0][0], &b[0][0], vec![]),
+                Diff::Equal(&a[0][1], &b[0][1], vec![]),
             ])
-        );
+        ]);
     }
 
     #[test]
     fn children_diff_1() {
-        let a = Tree::new(1, vec![Tree::new(2, vec![])]);
-        let b = Tree::new(1, vec![Tree::new(2, vec![]), Tree::new(3, vec![])]);
+        let a = vec![Tree::new(1, vec![Tree::new(2, vec![])])];
+        let b = vec![Tree::new(1, vec![Tree::new(2, vec![]), Tree::new(3, vec![])])];
 
-        assert_eq!(diff(&a, &b),
-            Diff::Equal(&a, &b, vec![
-                Diff::Equal(&a[0], &b[0], vec![]),
-                Diff::Right(&b[1]),
+        assert_eq!(diff(&a, &b), vec![
+            Diff::Equal(&a[0], &b[0], vec![
+                Diff::Equal(&a[0][0], &b[0][0], vec![]),
+                Diff::Right(&b[0][1]),
             ])
-        );
+        ]);
     }
 
     #[test]
     fn children_diff_2() {
-        let a = Tree::new(1, vec![Tree::new(2, vec![]), Tree::new(3, vec![])]);
-        let b = Tree::new(1, vec![Tree::new(2, vec![])]);
+        let a = vec![Tree::new(1, vec![Tree::new(2, vec![]), Tree::new(3, vec![])])];
+        let b = vec![Tree::new(1, vec![Tree::new(2, vec![])])];
 
-        assert_eq!(diff(&a, &b),
-            Diff::Equal(&a, &b, vec![
-                Diff::Equal(&a[0], &b[0], vec![]),
-                Diff::Left(&a[1]),
+        assert_eq!(diff(&a, &b), vec![
+            Diff::Equal(&a[0], &b[0], vec![
+                Diff::Equal(&a[0][0], &b[0][0], vec![]),
+                Diff::Left(&a[0][1]),
             ])
-        );
+        ]);
     }
 
     #[test]
     fn children_diff_3() {
-        let a = Tree::new(1, vec![Tree::new(2, vec![])]);
-        let b = Tree::new(1, vec![Tree::new(3, vec![])]);
+        let a = vec![Tree::new(1, vec![Tree::new(2, vec![])])];
+        let b = vec![Tree::new(1, vec![Tree::new(3, vec![])])];
 
-        assert_eq!(diff(&a, &b),
-            Diff::Equal(&a, &b, vec![
-                Diff::Replace(&a[0], &b[0]),
+        assert_eq!(diff(&a, &b), vec![
+            Diff::Equal(&a[0], &b[0], vec![
+                Diff::Left (&a[0][0]),
+                Diff::Right(&b[0][0]),
             ])
-        );
+        ]);
     }
 
     #[test]
     fn children_diff_deep() {
-        let a = Tree::new(1, vec![
-            Tree::new(2, vec![
-                Tree::new(3, vec![
-                    Tree::new(4, vec![]),
-                ]),
-                Tree::new(5, vec![
-                    Tree::new(6, vec![]),
-                    Tree::new(7, vec![]),
-                ]),
-                Tree::new(8, vec![
-                    Tree::new(9, vec![]),
-                    Tree::new(10, vec![]),
-                ]),
-                Tree::new(11, vec![]),
-            ]),
-            Tree::new(12, vec![
-                Tree::new(13, vec![
-                    Tree::new(14, vec![]),
-                ]),
-            ]),
-            Tree::new(15, vec![]),
-        ]);
-        let b = Tree::new(1, vec![
-            Tree::new(2, vec![
-                Tree::new(3, vec![
-                    Tree::new(4, vec![]),
-                    Tree::new(6, vec![]),
-                ]),
-                Tree::new(5, vec![
-                    Tree::new(7, vec![]),
-                ]),
-                Tree::new(8, vec![
-                    Tree::new(9, vec![
-                        Tree::new(20, vec![]),
-                        Tree::new(21, vec![
-                            Tree::new(22, vec![]),
-                        ]),
+        let a = vec![
+            Tree::new(1, vec![
+                Tree::new(2, vec![
+                    Tree::new(3, vec![
+                        Tree::new(4, vec![]),
                     ]),
-                    Tree::new(10, vec![]),
+                    Tree::new(5, vec![
+                        Tree::new(6, vec![]),
+                        Tree::new(7, vec![]),
+                    ]),
+                    Tree::new(8, vec![
+                        Tree::new(9, vec![]),
+                        Tree::new(10, vec![]),
+                    ]),
+                    Tree::new(11, vec![]),
                 ]),
-                Tree::new(11, vec![]),
-            ]),
-            Tree::new(18, vec![]),
-            Tree::new(12, vec![
-                Tree::new(13, vec![
-                    Tree::new(15, vec![]),
+                Tree::new(12, vec![
+                    Tree::new(13, vec![
+                        Tree::new(14, vec![]),
+                    ]),
                 ]),
+                Tree::new(15, vec![]),
             ]),
-            Tree::new(19, vec![]),
-            Tree::new(17, vec![]),
-            Tree::new(16, vec![]),
-        ]);
+        ];
+        let b = vec![
+            Tree::new(1, vec![
+                Tree::new(2, vec![
+                    Tree::new(3, vec![
+                        Tree::new(4, vec![]),
+                        Tree::new(6, vec![]),
+                    ]),
+                    Tree::new(5, vec![
+                        Tree::new(7, vec![]),
+                    ]),
+                    Tree::new(8, vec![
+                        Tree::new(9, vec![
+                            Tree::new(20, vec![]),
+                            Tree::new(21, vec![
+                                Tree::new(22, vec![]),
+                            ]),
+                        ]),
+                        Tree::new(10, vec![]),
+                    ]),
+                    Tree::new(11, vec![]),
+                ]),
+                Tree::new(18, vec![]),
+                Tree::new(12, vec![
+                    Tree::new(13, vec![
+                        Tree::new(15, vec![]),
+                    ]),
+                ]),
+                Tree::new(19, vec![]),
+                Tree::new(17, vec![]),
+                Tree::new(16, vec![]),
+            ]),
+        ];
 
-        assert_eq!(diff(&a, &b),
-            Diff::Equal(&a, &b, vec![
-                Diff::Equal(&a[0], &b[0], vec![
-                    Diff::Equal(&a[0][0], &b[0][0], vec![
-                        Diff::Equal(&a[0][0][0], &b[0][0][0], vec![]),
-                        Diff::Right(&b[0][0][1]),
+        assert_eq!(diff(&a, &b), vec![
+            Diff::Equal(&a[0], &b[0], vec![
+                Diff::Equal(&a[0][0], &b[0][0], vec![
+                    Diff::Equal(&a[0][0][0], &b[0][0][0], vec![
+                        Diff::Equal(&a[0][0][0][0], &b[0][0][0][0], vec![]),
+                        Diff::Right(&b[0][0][0][1]),
                     ]),
-                    Diff::Equal(&a[0][1], &b[0][1], vec![
-                        Diff::Left (&a[0][1][0]),
-                        Diff::Equal(&a[0][1][1], &b[0][1][0], vec![]),
+                    Diff::Equal(&a[0][0][1], &b[0][0][1], vec![
+                        Diff::Left (&a[0][0][1][0]),
+                        Diff::Equal(&a[0][0][1][1], &b[0][0][1][0], vec![]),
                     ]),
-                    Diff::Equal(&a[0][2], &b[0][2], vec![
-                        Diff::Equal(&a[0][2][0], &b[0][2][0], vec![
-                            Diff::Right(&b[0][2][0][0]),
-                            Diff::Right(&b[0][2][0][1]),
+                    Diff::Equal(&a[0][0][2], &b[0][0][2], vec![
+                        Diff::Equal(&a[0][0][2][0], &b[0][0][2][0], vec![
+                            Diff::Right(&b[0][0][2][0][0]),
+                            Diff::Right(&b[0][0][2][0][1]),
                         ]),
-                        Diff::Equal(&a[0][2][1], &b[0][2][1], vec![]),
+                        Diff::Equal(&a[0][0][2][1], &b[0][0][2][1], vec![]),
                     ]),
-                    Diff::Equal(&a[0][3], &b[0][3], vec![]),
+                    Diff::Equal(&a[0][0][3], &b[0][0][3], vec![]),
                 ]),
-                Diff::Right(&b[1]),
-                Diff::Equal(&a[1], &b[2], vec![
-                    Diff::Equal(&a[1][0], &b[2][0], vec![
-                        Diff::Replace(&a[1][0][0], &b[2][0][0]),
+                Diff::Right(&b[0][1]),
+                Diff::Equal(&a[0][1], &b[0][2], vec![
+                    Diff::Equal(&a[0][1][0], &b[0][2][0], vec![
+                        Diff::Left (&a[0][1][0][0]),
+                        Diff::Right(&b[0][2][0][0]),
                     ]),
                 ]),
-                Diff::Right(&b[3]),
-                Diff::Right(&b[4]),
-                Diff::Replace(&a[2], &b[5]),
-            ])
-        );
+                Diff::Left (&a[0][2]),
+                Diff::Right(&b[0][3]),
+                Diff::Right(&b[0][4]),
+                Diff::Right(&b[0][5]),
+            ]),
+        ]);
     }
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -472,7 +471,7 @@ A
 `- C
    `- (+) D");
     }
-
+/*
     #[test]
     fn print_diff_left() {
         let a = Tree::new("A", vec![Tree::new("B", vec![])]);
@@ -594,4 +593,5 @@ A
 |  `- (+) ?
 `- D");
     }
+*/
 }
