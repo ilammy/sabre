9e6f8a6cf0d6f32e9e72dbb7326b515b54f03a20
ilammy <a.lozovsky@gmail.com>
2017-12-23 16:43:44 +0200

    Custom Debug impl for ScannedDatum
    Data also can be formatted into its "external representation" in Scheme
    terms. Note, however, that certain data is not correctly rendered as it
    may require escaping (e.g., strings, symbols).

--- src/libreader/datum.rs
+++ src/libreader/datum.rs
@@ -9,12 +9,14 @@
 //!
 //! This module contains definitions of data recognized and processed by Scheme reader.
 
+use std::fmt;
+
 use locus::diagnostics::{Span};
 
 use intern_pool::Atom;
 
 /// A scanned datum with extents information. An AST node, if you with.
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(PartialEq, Eq, Clone)]
 pub struct ScannedDatum {
     /// The datum itself.
     pub value: DatumValue,
@@ -24,7 +26,7 @@ pub struct ScannedDatum {
 }
 
 /// Types of data recognized by the parser.
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(PartialEq, Eq, Clone)]
 pub enum DatumValue {
     /// Boolean literal.
     Boolean(bool),
@@ -58,4 +60,93 @@ pub enum DatumValue {
 
     /// Reference to a labeled datum.
     LabelReference(Atom),
+}
+
+impl fmt::Debug for ScannedDatum {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self.value)
+    }
+}
+
+impl fmt::Debug for DatumValue {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            DatumValue::Boolean(value) =>
+                write!(f, "{}", if value { "#t" } else { "#f" }),
+            DatumValue::Character(value) =>
+                write!(f, "#\\x{:04X}", value as u32),
+            DatumValue::Number(value) =>
+                write!(f, "{:?}", value),
+            DatumValue::String(value) =>
+                write!(f, "\"{:?}\"", value),
+            DatumValue::Symbol(value) =>
+                write!(f, "{:?}", value),
+            DatumValue::Bytevector(ref values) =>
+                write_list(f, "#u8(", values, " ", ")"),
+            DatumValue::Vector(ref values) =>
+                write_list(f, "#(", values, " ", ")"),
+            DatumValue::ProperList(ref values) =>
+                write_list(f, "(", values, " ", ")"),
+            DatumValue::DottedList(ref values) =>
+                write_dotted_list(f, "(", values, " ", ")"),
+            DatumValue::LabeledDatum(label, ref value) =>
+                write!(f, "#{:?}={:?}", label, value),
+            DatumValue::LabelReference(label) =>
+                write!(f, "#{:?}#", label),
+        }
+    }
+}
+
+fn write_list<I, T>(f: &mut fmt::Formatter, prefix: &str, items: I, sep: &str, suffix: &str)
+    -> fmt::Result
+    where I: IntoIterator<Item=T>,
+          T: fmt::Debug,
+{
+    let mut first = true;
+
+    try!(f.write_str(prefix));
+    for item in items {
+        if first {
+            first = false;
+        } else {
+            try!(f.write_str(sep));
+        }
+        try!(write!(f, "{:?}", item));
+    }
+    try!(f.write_str(suffix));
+
+    return Ok(());
+}
+
+fn write_dotted_list<I, T>(f: &mut fmt::Formatter, prefix: &str, items: I, sep: &str, suffix: &str)
+    -> fmt::Result
+    where I: IntoIterator<Item=T>,
+          T: fmt::Debug,
+{
+    let mut iter = items.into_iter();
+    let mut curr = iter.next();
+    let mut first = true;
+
+    try!(f.write_str(prefix));
+    while let Some(item) = curr {
+        let next = iter.next();
+
+        if first {
+            first = false;
+        } else {
+            if next.is_some() {
+                try!(f.write_str(sep));
+            } else {
+                try!(f.write_str(sep));
+                try!(f.write_str("."));
+                try!(f.write_str(sep));
+            }
+        }
+        try!(write!(f, "{:?}", item));
+
+        curr = next;
+    }
+    try!(f.write_str(suffix));
+
+    return Ok(());
 }
