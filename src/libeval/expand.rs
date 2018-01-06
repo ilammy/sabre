8c7953c0c8e6700ef453696618c3432e63d17d51
ilammy <a.lozovsky@gmail.com>
2018-01-06 09:10:55 +0200

    New core expander
    This implements a new expander entry point. It codifies the behavior of
    BasicExpander and ApplicationExpander as this is really the kernel
    expander behavior of Scheme. Literals always mean literals, forms always
    mean applications (when they are not macro uses), identifiers always
    mean variable references (as Scheme does not support identifier macros).
    Thus we can hardocode this behavior into the core expander itself rather
    than abstract it out in some BasicExpander implementing the Expander
    trait, which must be the lowest expander in the stack to work properly.

--- src/libeval/expand.rs
+++ src/libeval/expand.rs
@@ -14,11 +14,12 @@
 
 use std::rc::Rc;
 
-use locus::diagnostics::{Handler};
-use reader::datum::{ScannedDatum};
+use locus::diagnostics::{Handler, DiagnosticKind, Span};
+use reader::datum::{ScannedDatum, DatumValue};
+use reader::intern_pool::Atom;
 
-use environment::{Environment};
-use expression::{Expression};
+use environment::{Environment, ReferenceKind};
+use expression::{Expression, ExpressionKind, Literal};
 
 /// Trait of all macro expanders.
 pub trait Expander {
@@ -39,4 +40,136 @@ pub trait Expander {
         environment: &Rc<Environment>,
         diagnosic: &Handler,
     ) -> Expression;
+}
+
+/// Expand macros in the provided datum.
+///
+/// The provided environment is used to lookup macro definitions and to store defined variables.
+/// Any expansion errors are reported to the provided handler (the resulting expression may be
+/// garbled due to error recovery).
+pub fn expand(
+    datum: &ScannedDatum,
+    environment: &Rc<Environment>,
+    diagnostic: &Handler,
+) -> Expression {
+    match datum.value {
+        // Literal data does not contain any macros, it is retured as is.
+        DatumValue::Boolean(value) =>
+            literal(datum, environment, Literal::Boolean(value)),
+        DatumValue::Number(value) =>
+            literal(datum, environment, Literal::Number(value)),
+        DatumValue::Character(value) =>
+            literal(datum, environment, Literal::Character(value)),
+        DatumValue::String(value) =>
+            literal(datum, environment, Literal::String(value)),
+        DatumValue::Vector(ref elements) =>
+            literal(datum, environment, Literal::Vector(elements.clone())),
+        DatumValue::Bytevector(ref elements) =>
+            literal(datum, environment, Literal::Bytevector(elements.clone())),
+
+        // R7RS Scheme does not support identifier macros, so symbols always mean references.
+        DatumValue::Symbol(name) =>
+            reference(datum, environment, name),
+
+        // Lists denote forms, so proceed with macro expansion in them.
+        DatumValue::ProperList(ref terms) | DatumValue::DottedList(ref terms) =>
+            form(datum, terms, environment, diagnostic),
+
+        // Labeled data cannot be used in programs, only in literal quoted data.
+        // Report the error and assume the label never existed, reinvoking the expander.
+        DatumValue::LabeledDatum(_, ref labeled_datum) => {
+            diagnostic.report(DiagnosticKind::err_expand_datum_label,
+                Span::new(datum.span.from, labeled_datum.span.from));
+
+            expand(labeled_datum, environment, diagnostic)
+        }
+
+        // Datum labels cannot be used in programs, only in literal quoted data.
+        // Report the error and use some placeholder value instead.
+        DatumValue::LabelReference(_) => {
+            diagnostic.report(DiagnosticKind::err_expand_datum_label,
+                datum.span);
+
+            literal(datum, environment, Literal::Boolean(false))
+        }
+    }
+}
+
+/// Make a literal expansion result.
+fn literal(datum: &ScannedDatum, environment: &Rc<Environment>, value: Literal) -> Expression {
+    Expression {
+        kind: ExpressionKind::Literal(value),
+        span: datum.span,
+        environment: environment.clone(),
+    }
+}
+
+/// Make a reference expansion result.
+fn reference(datum: &ScannedDatum, environment: &Rc<Environment>, name: Atom) -> Expression {
+    Expression {
+        kind: ExpressionKind::Reference(name),
+        span: datum.span,
+        environment: environment.clone(),
+    }
+}
+
+/// Expand macros in a form.
+fn form(
+    datum: &ScannedDatum,
+    terms: &[ScannedDatum],
+    environment: &Rc<Environment>,
+    diagnosic: &Handler,
+) -> Expression {
+    // There are three forms of forms in Scheme.
+    //
+    // The first one is (), an empty list. It is invalid in Scheme and has no prescribed meaning.
+    // Report an error and return some placeholder value.
+    if terms.is_empty() {
+        diagnosic.report(DiagnosticKind::err_expand_invalid_application,
+            Span::new(datum.span.from + 1, datum.span.to - 1));
+        return Expression {
+            kind: ExpressionKind::Application(vec![]),
+            span: datum.span,
+            environment: environment.clone(),
+        };
+    }
+
+    // Macro uses always have a (<keyword> . <data>) form where <keyword> is an identifier bound
+    // to a syntactic keyword, and <data> is a proper, improper, or empty list of data. It is
+    // the binding which distinguishes a macro use from a procedure call (an application).
+    if let Some(expander) = is_macro_use(terms, environment) {
+        return expander.expand_form(datum, environment, diagnosic);
+    }
+
+    // Applications have a form (procedure arguments ...) which must be a proper list. Report
+    // dotted lists and expand all terms before placing them into a corresponding expression.
+    if let DatumValue::DottedList(..) = datum.value {
+        assert!(terms.len() >= 2);
+        let last = terms.len() - 1;
+        diagnosic.report(DiagnosticKind::err_expand_invalid_application,
+            Span::new(terms[last - 1].span.to, terms[last].span.from));
+    }
+
+    let expressions = terms.iter()
+        .map(|datum| expand(datum, environment, diagnosic))
+        .collect();
+
+    return Expression {
+        kind: ExpressionKind::Application(expressions),
+        span: datum.span,
+        environment: environment.clone(),
+    };
+}
+
+/// Check if terms denote a macro use. Return the corresponding expander if they are.
+fn is_macro_use<'a>(terms: &[ScannedDatum], environment: &'a Environment) -> Option<&'a Expander> {
+    assert!(terms.len() >= 1);
+    if let DatumValue::Symbol(name) = terms[0].value {
+        if let Some(variable) = environment.resolve_variable(name) {
+            if let ReferenceKind::Syntactic { expander } = variable.kind {
+                return Some(expander);
+            }
+        }
+    }
+    return None;
 }
