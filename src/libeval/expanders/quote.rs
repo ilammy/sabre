f88bd82a32a654a75365c509298678920c869320
ilammy <a.lozovsky@gmail.com>
2018-01-07 01:01:16 +0200

    New expander for (quote datum)
    Also mostly self-explanatory.
    
    Note the placeholder value computation. It is performed only when
    necessary, otherwise missing_last_span() will panic.
    
    I do not really like the approach with these arbitrary '#f's used as
    placeholders. This should change in the future.

--- src/libeval/expanders/quote.rs
+++ src/libeval/expanders/quote.rs
@@ -15,7 +15,8 @@ use reader::intern_pool::{Atom};
 
 use environment::{Environment};
 use expression::{Expression, ExpressionKind};
-use expanders::{Expander, ExpansionResult};
+use expand::Expander;
+use expanders::{Expander as OldExpander, ExpansionResult};
 
 /// Expand `quote` special forms into quotations.
 pub struct QuoteExpander {
@@ -32,8 +33,8 @@ impl QuoteExpander {
     }
 }
 
-impl Expander for QuoteExpander {
-    fn expand(&self, datum: &ScannedDatum, environment: &Rc<Environment>, diagnostic: &Handler, _expand: &Expander) -> ExpansionResult {
+impl OldExpander for QuoteExpander {
+    fn expand(&self, datum: &ScannedDatum, environment: &Rc<Environment>, diagnostic: &Handler, _expand: &OldExpander) -> ExpansionResult {
         use expanders::utils::{is_named_form, expect_list_length_fixed};
 
         // Filter out anything that certainly does not look as a quote form.
@@ -63,4 +64,57 @@ impl Expander for QuoteExpander {
             environment: environment.clone(),
         });
     }
+}
+
+impl Expander for QuoteExpander {
+    fn expand_form(
+        &self,
+        datum: &ScannedDatum,
+        environment: &Rc<Environment>,
+        diagnostic: &Handler,
+    ) -> Expression {
+        use expanders::utils::missing_last_span;
+
+        // The only valid form is (quote datum). Do not expand anything. Use the first datum
+        // available or pull some placeholder out of thin air if this is a (quote) form.
+        let data = expect_quote_form(self.name, datum, diagnostic);
+
+        let quoted_datum = data.first().cloned().unwrap_or_else(|| ScannedDatum {
+            value: DatumValue::Boolean(false),
+            span: missing_last_span(datum),
+        });
+
+        return Expression {
+            kind: ExpressionKind::Quotation(quoted_datum),
+            span: datum.span,
+            environment: environment.clone(),
+        };
+    }
+}
+
+fn expect_quote_form<'a>(
+    keyword: Atom,
+    datum: &'a ScannedDatum,
+    diagnostic: &Handler,
+) -> &'a [ScannedDatum] {
+    use expanders::utils::{expect_form, missing_last_span};
+
+    let (dotted, terms) = expect_form(keyword, datum);
+    let last = terms.len() - 1;
+
+    if terms.len() < 2 {
+        diagnostic.report(DiagnosticKind::err_expand_invalid_quote, missing_last_span(datum));
+    }
+    if terms.len() > 2 {
+        let extra_forms = Span::new(terms[2].span.from, terms[last].span.to);
+        diagnostic.report(DiagnosticKind::err_expand_invalid_quote, extra_forms);
+    }
+
+    if dotted && (terms.len() == 2) {
+        assert!(terms.len() >= 2);
+        let around_dot = Span::new(terms[last - 1].span.to, terms[last].span.from);
+        diagnostic.report(DiagnosticKind::err_expand_invalid_quote, around_dot);
+    }
+
+    return &terms[1..];
 }
